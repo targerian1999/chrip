4c0b1cd6307138dc3f1b64371b73651547c22a6f
Muluneh Derese <mulunehd@ethiopianairlines.com>
2023-04-18 15:37:59 +0300

    new files for routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,145 +11,150 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "y/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime)
 
 const CreatePostWizard = () => {
-  const { user } = useUser();
+    const { user } = useUser();
 
-  const ctx = api.useContext();
+    const ctx = api.useContext();
 
-  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
-    onSuccess: () => {
-      setInput("");
-      void ctx.posts.getAll.invalidate();
-    },
-    onError: (e) => {
-      const errorMessage = e.data?.zodError?.fieldErrors.content
-      if (errorMessage && errorMessage[0]) {
-        toast.error(errorMessage[0])
-      } else {
-        toast.error("Failed to post! Please try again later")
-      }
-    }
-  });
-
-  const [input, setInput] = useState<string>("");
-
-  if (!user) return null;
-
-  return (
-    <div className="flex gap-3 w-full">
-      <Image
-        src={user.profileImageUrl}
-        alt="profile iamge"
-        className="w-14 h-14 rounded-full"
-        width="56"
-        height="56"
-      />
-
-      <input
-        placeholder="Type some emojis!"
-        className="bg-transparent grow outline-none"
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onKeyDown={(e) => {
-          if (e.key === "Enter") {
-            e.preventDefault();
-            if (input !== "") {
-              mutate({ content: input });
+    const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+        onSuccess: () => {
+            setInput("");
+            void ctx.posts.getAll.invalidate();
+        },
+        onError: (e) => {
+            const errorMessage = e.data?.zodError?.fieldErrors.content
+            if (errorMessage && errorMessage[0]) {
+                toast.error(errorMessage[0])
+            } else {
+                toast.error("Failed to post! Please try again later")
             }
-          }
-        }}
-        disabled={isPosting}
-      />
-      {input !== "" && !isPosting && (
-        <button onClick={() => mutate({ content: input })} >
-          Post
-        </button>)}
+        }
+    });
 
-      {isPosting &&
-        <div className="flex justify-center items-center">
-          <LoadingSpinner size={20} />
+    const [input, setInput] = useState<string>("");
+
+    if (!user) return null;
+
+    return (
+        <div className="flex gap-3 w-full">
+            <Image
+                src={user.profileImageUrl}
+                alt="profile iamge"
+                className="w-14 h-14 rounded-full"
+                width="56"
+                height="56"
+            />
+
+            <input
+                placeholder="Type some emojis!"
+                className="bg-transparent grow outline-none"
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                onKeyDown={(e) => {
+                    if (e.key === "Enter") {
+                        e.preventDefault();
+                        if (input !== "") {
+                            mutate({ content: input });
+                        }
+                    }
+                }}
+                disabled={isPosting}
+            />
+            {input !== "" && !isPosting && (
+                <button onClick={() => mutate({ content: input })} >
+                    Post
+                </button>)}
+
+            {isPosting &&
+                <div className="flex justify-center items-center">
+                    <LoadingSpinner size={20} />
+                </div>
+            }
         </div>
-      }
-    </div>
-  )
+    )
 }
 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number]
 
 const PostView = (props: PostWithUser) => {
-  const { post, author } = props
-  return (
-    <div key={post.id} className="p-4 border-b border-slate-400 flex gap-3">
-      <Image
-        src={author.profileImageUrl}
-        alt={`${author.username}'s profile picture`}
-        className="w-14 h-14 rounded-full"
-        width="56"
-        height="56"
-      />
+    const { post, author } = props
+    return (
+        <div key={post.id} className="p-4 border-b border-slate-400 flex gap-3">
+            <Image
+                src={author.profileImageUrl}
+                alt={`${author.username}'s profile picture`}
+                className="w-14 h-14 rounded-full"
+                width="56"
+                height="56"
+            />
 
-      <div className="flex flex-col">
-        <div className="flex text-slate-300 gap-1">
-          <span>{`@${author.username} `}</span>
-          <span className="font-thin">{`. ${dayjs(post.createdAt).fromNow()}`}</span>
+            <div className="flex flex-col">
+                <div className="flex text-slate-300 gap-1">
+                    <Link href={`/@${author.username}`}>
+                        <span>{`@${author.username} `}</span>
+                    </Link>
+                    <Link href={`/post/${post.id}`}>
+                        <span className="font-thin">{`. ${dayjs(post.createdAt).fromNow()}`}</span>
+                    </Link>
+                </div>
+
+                <span className="text-2xl">
+                    {post.content}
+                </span>
+            </div>
         </div>
-
-        <span className="text-2xl">
-          {post.content}
-        </span>
-      </div>
-    </div>
-  )
+    )
 }
 
 const Feed = () => {
-  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
+    const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
-  if (postsLoading) return <LoadingPage />;
+    if (postsLoading) return <LoadingPage />;
 
-  if (!data) return <div>Something went wrong</div>;
+    if (!data) return <div>Something went wrong</div>;
 
-  return (
-    <div className="flex flex-col">
-      {data.map((fullPost) => (
-        <PostView {...fullPost} key={fullPost.post.id} />
-      ))}
-    </div>
-  )
+    return (
+        <div className="flex flex-col">
+            {data.map((fullPost) => (
+                <PostView {...fullPost} key={fullPost.post.id} />
+            ))}
+        </div>
+    )
 }
 
 const Home: NextPage = () => {
-  const { isLoaded: userLoaded, isSignedIn } = useUser();
+    const { isLoaded: userLoaded, isSignedIn } = useUser();
 
-  // start fetching ASAP
-  api.posts.getAll.useQuery();
+    // start fetching ASAP
+    api.posts.getAll.useQuery();
 
-  // Return empty div if user isn't loaded yet
-  if (!userLoaded) return <div />;
+    // Return empty div if user isn't loaded yet
+    if (!userLoaded) return <div />;
 
-  return (
-    <>
-      <Head>
-        <title>Create T3 App</title>
-        <meta name="description" content="Generated by create-t3-app" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className="flex justify-center h-screen">
-        <div className="h-full w-full md:max-w-2xl border-x border-slate-400">
-          <div className="border-b border-slate-400 p-4 flex">
-            {!isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
-            {isSignedIn && <CreatePostWizard />}
-          </div>
+    return (
+        <>
+            <Head>
+                <title>Create T3 App</title>
+                <meta name="description" content="Generated by create-t3-app" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className="flex justify-center h-screen">
+                <div className="h-full w-full md:max-w-2xl border-x border-slate-400">
+                    <div className="border-b border-slate-400 p-4 flex">
+                        {!isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
+                        {isSignedIn && <CreatePostWizard />}
+                    </div>
 
-          <Feed />
-        </div>
-      </main>
-    </>
-  );
+                    <Feed />
+                </div>
+            </main>
+        </>
+    );
 };
 
 export default Home;
