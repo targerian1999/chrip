f813288d170532e4434c01296ebe807d7b6e5245
Muluneh Derese <mulunehd@ethiopianairlines.com>
2023-04-18 10:41:07 +0300

    add auth

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export default withClerkMiddleware((req: NextRequest) => {
+    return NextResponse.next();
+});
+
+export const config = {
+    matcher: [
+        "/((?!static|.*\\..*|_next|favicon.ico).*)",
+        "/",
+    ],
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { type AppType } from "next/app";
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { api } from "y/utils/api";
 
 import "y/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
 };
 
 export default api.withTRPC(MyApp);
